a0c7f8fc202cdb5cc35d197d06ba952a8bc1a01f
kanechu <kanechu@itdept.com.hk>
2014-03-12 16:22:35 +0800

    fix -connection Ari Export Button

--- worldtrans/TrackHomeController.h
+++ worldtrans/TrackHomeController.h
@@ -12,6 +12,8 @@
 
 @property (weak, nonatomic) IBOutlet UIButton *lbtn_exhbl_search;
 
+
+@property (weak, nonatomic) IBOutlet UIButton *lbtn_exhbl_AirSearch;
 @property (weak, nonatomic) IBOutlet UITextField *ltf_search_no;
 
 @end
