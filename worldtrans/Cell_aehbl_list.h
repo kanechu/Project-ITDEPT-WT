d613cab198783ac4ffbf584077fee789f927dacd
kanechu <kanechu@itdept.com.hk>
2014-03-13 17:40:49 +0800

    add -custom UITableViewCell for AehblListControler

---
+++ worldtrans/Cell_aehbl_list.h
@@ -0,0 +1,17 @@
+//
+//  Cell_aehbl_list.h
+//  worldtrans
+//
+//  Created by itdept on 14-3-13.
+//  Copyright (c) 2014年 itdept. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Cell_aehbl_list : UITableViewCell
+@property (nonatomic) IBOutlet UILabel *ilb_so_no;
+@property ( nonatomic) IBOutlet UILabel *ilb_date;
+
+@property ( nonatomic) IBOutlet UILabel *ilb_hbl_no;
+
+@end
