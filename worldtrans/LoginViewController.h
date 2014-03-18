3d3d6d1ba3f4d340214ce14f2d7e6831e2d093d1
kanechu <kanechu@itdept.com.hk>
2014-03-18 17:49:29 +0800

    add -loginUI

---
+++ worldtrans/LoginViewController.h
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.h
+//  worldtrans
+//
+//  Created by itdept on 14-3-18.
+//  Copyright (c) 2014年 itdept. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class TrackHomeController;
+@interface LoginViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *user_Password;
+@property (weak, nonatomic) IBOutlet UITextField *user_ID;
+- (IBAction)UserLogin:(id)sender;
+- (IBAction)closeLoginUI:(id)sender;
+
+@end
---
+++ worldtrans/LoginViewController.m
@@ -0,0 +1,46 @@
+//
+//  LoginViewController.m
+//  worldtrans
+//
+//  Created by itdept on 14-3-18.
+//  Copyright (c) 2014年 itdept. All rights reserved.
+//
+
+#import "LoginViewController.h"
+#import "TrackHomeController.h"
+#import "MZFormSheetController.h"
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)UserLogin:(id)sender {
+    [self mz_dismissFormSheetControllerAnimated:YES completionHandler:^(MZFormSheetController* formSheet){}];
+}
+
+- (IBAction)closeLoginUI:(id)sender {
+     [self mz_dismissFormSheetControllerAnimated:YES completionHandler:^(MZFormSheetController* formSheet){}];
+}
+@end
