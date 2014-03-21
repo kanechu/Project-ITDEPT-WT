a54406078270c1c53a334cc20dbc1263c293b572
kanechu <kanechu@itdept.com.hk>
2014-03-21 12:02:38 +0800

    fix -After landing,The user to obtain a more comprehensive data

--- worldtrans/AehblGeneralController.h
+++ worldtrans/AehblGeneralController.h
@@ -7,10 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "DB_login.h"
 
 @interface AehblGeneralController : UITableViewController
 @property(nonatomic) NSString *is_search_column;
 @property(nonatomic) NSString *is_search_value;
 
 @property (strong,nonatomic) NSMutableArray *ilist_aehbl;
+@property (strong,nonatomic)DB_login *dbLogin;
 @end
