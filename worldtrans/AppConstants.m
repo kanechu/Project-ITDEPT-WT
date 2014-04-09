7c29a432735525b6c41295389a79fa6b44064c8d
kanechu <itdept@itdepts-MacBook-Pro.local>
2014-04-08 19:46:05 -0700

    change to prod path

--- worldtrans/AppConstants.m
+++ worldtrans/AppConstants.m
@@ -16,7 +16,7 @@ NSString* const STR_LOGIN_URL=@"itwebapi/api/users/login";
 NSString* const STR_ALERT_URL=@"itwebapi/api/cargotracking/alert";
 NSString* const STR_MILESTONE_URL=@"itwebapi/api/cargotracking/milestone";
 #else
-NSString* const STR_BASE_URL = @"http://demo.itdept.com.hk/";
+NSString* const STR_BASE_URL = @"http://223.255.167.158/";
 NSString* const STR_SEA_URL =@"itwebapi/api/cargotracking/exhbl";
 NSString* const STR_AIR_URL =@"itwebapi/api/cargotracking/aehbl";
 NSString* const STR_LOGIN_URL=@"itwebapi/api/users/login";
