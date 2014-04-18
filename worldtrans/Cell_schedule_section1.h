a4b6158f4f6858d362601ac1effc487b24f9363b
kanechu <kanechu@itdept.com.hk>
2014-04-18 15:53:22 +0800

    add custom UITableViewCell

---
+++ worldtrans/Cell_schedule_section1.h
@@ -0,0 +1,16 @@
+//
+//  Cell_schedule_section1.h
+//  worldtrans
+//
+//  Created by itdept on 14-4-18.
+//  Copyright (c) 2014年 Worldtrans Logistics Services Ltd. . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Custom_TextField.h"
+@interface Cell_schedule_section1 : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *ilb_btn_image;
+@property (weak, nonatomic) IBOutlet UILabel *ilb_port;
+@property (weak, nonatomic) IBOutlet Custom_TextField *ilb_show_portName;
+
+@end
---
+++ worldtrans/Cell_schedule_section2_row1.h
@@ -0,0 +1,17 @@
+//
+//  Cell_schedule_section2_row1.h
+//  worldtrans
+//
+//  Created by itdept on 14-4-18.
+//  Copyright (c) 2014年 Worldtrans Logistics Services Ltd. . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Custom_TextField.h"
+@interface Cell_schedule_section2_row1 : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *ii_calendar_img;
+
+@property (weak, nonatomic) IBOutlet Custom_TextField *itf_show_dateType;
+@property (weak, nonatomic) IBOutlet UIButton *ibt_drop_btn;
+
+@end
