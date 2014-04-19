fdb99db3cfd3aad810c2476c80062ec9360de53d
kanechu <kanechu@itdept.com.hk>
2014-04-19 11:04:49 +0800

    add r schedule path

--- worldtrans/AppConstants.h
+++ worldtrans/AppConstants.h
@@ -25,4 +25,5 @@ extern NSString* const STR_AIR_URL;
 extern NSString* const STR_LOGIN_URL;
 extern NSString* const STR_ALERT_URL;
 extern NSString* const STR_MILESTONE_URL;
+extern NSString* const STR_SCHEDULE_URL;
 @end
