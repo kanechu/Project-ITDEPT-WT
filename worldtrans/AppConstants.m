3dc7007c461434252e54a2c47ca97d4e014b18f1
kanechu <kanechu@itdept.com.hk>
2014-04-21 14:03:56 +0800

    fix -change path

--- worldtrans/AppConstants.m
+++ worldtrans/AppConstants.m
@@ -15,7 +15,7 @@ NSString* const STR_AIR_URL =@"itleo.web/api/cargotracking/aehbl";
 NSString* const STR_LOGIN_URL=@"itleo.web/api/users/login";
 NSString* const STR_ALERT_URL=@"itleo.web/api/cargotracking/alert";
 NSString* const STR_MILESTONE_URL=@"itleo.web/api/cargotracking/milestone";
-NSString* const STR_SCHEDULE_URL=@"itleo.web/chargotracking/schedule";
+NSString* const STR_SCHEDULE_URL=@"itleo.web/api/cargotracking/schedule";
 #else
 NSString* const STR_BASE_URL = @"http://223.255.167.158/";
 NSString* const STR_SEA_URL =@"itwebapi/api/cargotracking/exhbl";
