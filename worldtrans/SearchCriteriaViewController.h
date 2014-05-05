488f103b827c3839340eca883832094434a0d134
kanechu <kanechu@itdept.com.hk>
2014-05-05 16:51:28 +0800

    add a searchCriteriaViewController  class

---
+++ worldtrans/SearchCriteriaViewController.h
@@ -0,0 +1,15 @@
+//
+//  SearchCriteriaViewController.h
+//  worldtrans
+//
+//  Created by itdept on 14-5-5.
+//  Copyright (c) 2014年 Worldtrans Logistics Services Ltd. . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchCriteriaViewController : UITableViewController
+
+@property (strong,nonatomic)NSMutableArray *alist_searchCriteria;
+
+@end
