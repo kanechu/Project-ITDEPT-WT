af6da27d4389584fc7889ec4769938731c1b4ce4
kanechu <itdept@itdepts-MacBook-Pro.local>
2014-11-21 18:19:21 -0800

    version control

--- worldtrans/AppConstants.m
+++ worldtrans/AppConstants.m
@@ -8,7 +8,7 @@
 
 #import "AppConstants.h"
 
-#ifdef DEBUG 
+//#ifdef DEBUG
 NSString* const STR_BASE_URL = @"http://223.255.167.158/itwebapi_v2";
 NSString* const STR_LOGIN_URL=@"api/users/login";
 NSString* const STR_SYPARA_URL=@"api/System/sypara";
@@ -22,14 +22,14 @@ NSString* const STR_PORTNAME_URL=@"api/master/maport";
 NSString* const STR_SEARCHCRITERIA_URL=@"api/search/searchcriteria";
 NSString* const STR_ICON_URL=@"api/system/icon";
 NSString* const STR_EXCNTR_STATUS_URL=@"api/cargotracking/excntr_status";
-#else
+//#else
 //worldtrans company
-NSString* const STR_BASE_URL = @"http://223.255.167.158/itwebapi/";
+//NSString* const STR_BASE_URL = @"http://223.255.167.158/itwebapi/";
 
 //itdept company
-NSString* const STR_BASE_URL = @"http://demo.itdept.com.hk/itleo.web/";
+//NSString* const STR_BASE_URL = @"http://demo.itdept.com.hk/itleo.web/";
 
 //kie
-NSString* const STR_BASE_URL = @"http://192.168.1.17/kie_web_api/";
+//NSString* const STR_BASE_URL = @"http://192.168.1.17/kie_web_api/";
 
-#endif
+//#endif
